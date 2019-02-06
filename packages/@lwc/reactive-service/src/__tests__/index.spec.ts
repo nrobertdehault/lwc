915ae0c555c4d10ebfd0c8827b8cf8cc22cf179f
Caridy Patiño <caridy@gmail.com>
2019-02-06 14:31:36 -0500

    fix(engine): issue #858 to make setters reactive behind a flag

---
+++ packages/@lwc/reactive-service/src/__tests__/index.spec.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+// import {
+//     setCurrentObserver,
+//     getCurrentObserver,
+//     getProxy,
+//     getReadOnlyProxy,
+//     unwrapProxy,
+// } from '../index';
+
+describe('reactive service', () => {
+    describe('setCurrentObserver()', () => {
+        // TBD
+    });
+
+    describe('getCurrentObserver()', () => {
+        // TBD
+    });
+
+    describe('getProxy()', () => {
+        // TBD
+    });
+
+    describe('getReadOnlyProxy()', () => {
+        // TBD
+    });
+
+    describe('unwrapProxy()', () => {
+        // TBD
+    });
+});
